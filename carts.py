dfeedaed2ef01ba244318fe116de4916f950725a
joe-doe <joe.doe@engineer.com>
2016-07-05 13:19:36 +0300

    initial commit

---
+++ carts.py
@@ -0,0 +1,7 @@
+cart_1 = [
+        'https://www.skroutz.gr/s/3385413/Lamberts-Ginkgo-Biloba-Extract-6000mg-180-%CF%84%CE%B1%CE%BC%CF%80%CE%BB%CE%AD%CF%84%CE%B5%CF%82.html',
+        'https://www.skroutz.gr/s/4038086/Lamberts-Vitamin-D3-4000iu-120-%CE%BA%CE%AC%CF%88%CE%BF%CF%85%CE%BB%CE%B5%CF%82.html',
+        'https://www.skroutz.gr/s/3486811/Merck-%CE%9C%CE%B9%CE%B1-%CE%A4%CE%B7%CE%BD-%CE%97%CE%BC%CE%AD%CF%81%CE%B1-%CE%9A%CE%B1%CE%B8%CE%B1%CF%81%CF%8C-%CE%9C%CE%BF%CF%85%CF%81%CE%BF%CF%85%CE%BD%CE%AD%CE%BB%CE%B1%CE%B9%CE%BF-60-%CE%BA%CE%AC%CF%88%CE%BF%CF%85%CE%BB%CE%B5%CF%82.html',
+        'https://www.skroutz.gr/s/5500366/Lanes-Multi-Memory-30-%CF%84%CE%B1%CE%BC%CF%80%CE%BB%CE%AD%CF%84%CE%B5%CF%82.html',
+        'https://www.skroutz.gr/s/3508181/Nature-s-Bounty-%CE%A3%CF%85%CE%BD%CE%AD%CE%BD%CE%B6%CF%85%CE%BC%CE%BF-Q10-30mg-Q-Sorb-50-%CE%BC%CE%B1%CE%BB%CE%B1%CE%BA%CE%AD%CF%82-%CE%BA%CE%AC%CF%88%CE%BF%CF%85%CE%BB%CE%B5%CF%82.html'
+    ]
